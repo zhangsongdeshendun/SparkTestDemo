63f60550c86b4ba5a6120df6e68bb5b0a15946f0
song.zhang <zhangs@inke.cn>
2018-01-06 22:14:01 +0800

    spark-flume-pull方式成功实现

--- src/main/scala/com/song/net/FlumePullWordCount.scala
+++ src/main/scala/com/song/net/FlumePullWordCount.scala
@@ -1,14 +1,15 @@
 package com.song.net
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.flume.FlumeUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.flume.FlumeUtils
 
 /**
-  * Spark Streaming整合Flume的第一种方式 采用push （这种方式是flume把数据推给spark，数据容易丢失）
+  * Spark Streaming整合Flume的第二种方式 采用pull （这种方式是spark从自定义的sink中拉数据，数据会在spark中有三个副本，数据不容易丢失）
+  *
   */
 
-object FlumePushWordCount {
+object FlumePullWordCount {
 
   def main(args: Array[String]): Unit = {
 
@@ -19,12 +20,12 @@ object FlumePushWordCount {
 
     val Array(hostname,port)=args//hostname=0.0.0.0 port=41414
 
-    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FlumePushWordCount")
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FlumePullWordCount")
 
     val ssc = new StreamingContext(sparkConf, Seconds(5))
 
 
-    val flumeStream=FlumeUtils.createStream(ssc,hostname,port.toInt)
+    val flumeStream=FlumeUtils.createPollingStream(ssc,hostname,port.toInt)
 
     flumeStream.map(x=>new String(x.event.getBody.array()).trim)
       .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).print()
