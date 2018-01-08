29ce342520d2ffc16d6adb8f9f5efd2d160fe5d8
song.zhang <zhangs@inke.cn>
2018-01-08 15:46:29 +0800

    完成了整个流程

--- src/main/scala/com/song/app/KafkaStringAppp.scala
+++ src/main/scala/com/song/app/KafkaStringAppp.scala
@@ -1,16 +1,15 @@
 package com.song.app
 
+import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import kafka.serializer.StringDecoder
 import org.apache.spark.streaming.kafka.KafkaUtils
 
-
 /**
-  * spark streaming 对接kafka的方式二，基于direct（生产环境用）
+  * 采用Direct方式消费kafka过来的数据
   */
 
-object KafkaDirectWordCount {
+object KafkaStringAppp {
 
   def main(args: Array[String]): Unit = {
     //两个参数是localhost:9092 streamtopic
@@ -21,7 +20,7 @@ object KafkaDirectWordCount {
 
     val Array(brokers, topics) = args
 
-    val sparkConf = new SparkConf() .setAppName("KafkaDirectWordCount").setMaster("local[2]")
+    val sparkConf = new SparkConf().setAppName("KafkaStringAppp").setMaster("local[2]")
 
     val ssc = new StreamingContext(sparkConf, Seconds(5))
 
@@ -34,7 +33,7 @@ object KafkaDirectWordCount {
     )
 
     // TODO... 自己去测试为什么要取第二个
-    messages.map(_._2).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).print()
+    messages.map(_._2).count().print()
 
     ssc.start()
     ssc.awaitTermination()
